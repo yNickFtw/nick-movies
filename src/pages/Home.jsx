70a0b1644aafb3c3394d75aecc6b9f444dfdd26f
yNickFtw <nkxynickftw@gmail.com>
2023-01-11 01:01:53 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import "./MovieGrid.css";
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Filmes mais bem avaliados</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregado...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
